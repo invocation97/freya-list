c855616be4bcc8724b32eaafcaafed7397504db9
invocation97 <danilo97benovic@gmail.com>
2025-12-31 17:05:50 +0100

    Set up better auth with the api and added a google oauth provider

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,15 +1,17 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from '@thallesp/nestjs-better-auth';
 import { TRPCModule } from 'nestjs-trpc';
-
+import { auth } from '@/lib/auth';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      envFilePath: '.env.local'
-    }),
+    ConfigModule.forRoot(),
     TRPCModule.forRoot({
       autoSchemaFile: '../../packages/api-client/src/server'
+    }),
+    AuthModule.forRoot({
+      auth
     })
   ],
 })
