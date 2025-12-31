a9c355b5e3bc8e7498845800645723403bf8e945
invocation97 <danilo97benovic@gmail.com>
2025-12-31 16:33:23 +0100

    Added Prisma, and set up the automatically generating client

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { TRPCModule } from 'nestjs-trpc';
 
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env.local'
+    }),
     TRPCModule.forRoot({
       autoSchemaFile: '../../packages/api-client/src/server'
     })
